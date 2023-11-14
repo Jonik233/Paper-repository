e607d0f42c1b1af0ba087f3e8422bdeef1bb1777
Vlad Mashtalier <vmashtaler5@gmail.com>
2023-11-14 14:54:27 +0200

    migration conducted for Articles model

--- paper_repository/main/models.py
+++ paper_repository/main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Articles(models.Model):
+    title = models.CharField('Title', max_length=500)
+    author = models.CharField('Author', max_length=255)
+    content = models.TextField('Content')
+    rating_points = models.IntegerField()
+    
+    CATEGORY_CHOICES = [
+        ("physics", "Physics"), 
+        ("chemistry", "Chemistry"),
+        ("economics", "Economics"),
+        ("history", "History"),
+        ("math", "Math"),
+        ("biology", "Biology"),
+        ("machine learning", "Machine Learning")
+    ]
+    field = models.CharField('Field', max_length=100, choices=CATEGORY_CHOICES)
+    
+    def __str__(self):
+        return self.title
