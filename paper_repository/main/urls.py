b810612150055a42c12884a162c7a612c57e093b
Vlad Mashtalier <vmashtaler5@gmail.com>
2023-11-16 13:05:28 +0200

    search app added

--- paper_repository/main/urls.py
+++ paper_repository/main/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 import registration.views
+import search.views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('registration/', registration.views.registration, name='registration'),
-    path('login/', registration.views.login, name='login')
+    path('login/', registration.views.login, name='login'),
+    path('search/', search.views.search, name='search')
 ]
