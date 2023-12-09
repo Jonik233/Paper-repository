0a7ecb672ec14aecb135521c228ff2c7c9a6d30e
Vlad Mashtalier <vmashtaler5@gmail.com>
2023-12-10 01:12:31 +0200

    search enabled

--- paper_repository/search/urls.py
+++ paper_repository/search/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('search/', views.search, name='search'),
+    path('search_results/', views.search_results, name='search_results')
 ]
--- paper_repository/search/views.py
+++ paper_repository/search/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from main.models import Articles
 
 def search(request):
     return render(request, 'search\search.html')
+
+def search_results(request):
+    query = request.GET.get('query', '')
+    if query:
+        articles = Articles.objects.filter(title__icontains=query)
+    else:
+        articles = Articles.objects.none()
+    
+    return render(request, 'search\papers.html', {"articles":articles})
