13fae7984699785294988d980c2df93edf28c073
Vlad Mashtalier <vmashtaler5@gmail.com>
2023-12-10 02:07:48 +0200

    search by field function added

--- paper_repository/search/views.py
+++ paper_repository/search/views.py
@@ -6,9 +6,24 @@ def search(request):
 
 def search_results(request):
     query = request.GET.get('query', '')
+    selected_field = request.GET.get('selected_field', '')
+
     if query:
-        articles = Articles.objects.filter(title__icontains=query)
+        if selected_field == "Physics":  
+            articles = Articles.objects.filter(title__icontains=query, field='physics')
+        elif selected_field == "Math":  
+            articles = Articles.objects.filter(title__icontains=query, field='math')
+        elif selected_field == "Economics":  
+            articles = Articles.objects.filter(title__icontains=query, field='economics')
+        elif selected_field == "Chemistry":  
+            articles = Articles.objects.filter(title__icontains=query, field='chemistry')
+        elif selected_field == "Biology":  
+            articles = Articles.objects.filter(title__icontains=query, field='biology')
+        elif selected_field == "Machine Learning":  
+            articles = Articles.objects.filter(title__icontains=query, field='machine learning')
+        else:
+            articles = Articles.objects.filter(title__icontains=query)
     else:
         articles = Articles.objects.none()
-    
-    return render(request, 'search\papers.html', {"articles":articles})
+
+    return render(request, 'search/papers.html', {"articles": articles})
