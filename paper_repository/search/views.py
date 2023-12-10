0d6ab82563fded5f08dab7253bdf49df03b26e20
Vlad Mashtalier <vmashtaler5@gmail.com>
2023-12-10 15:52:24 +0200

    search by field method refactored

--- paper_repository/search/views.py
+++ paper_repository/search/views.py
@@ -7,23 +7,16 @@ def search(request):
 def search_results(request):
     query = request.GET.get('query', '')
     selected_field = request.GET.get('selected_field', '')
+    articles = Articles.objects.none()
 
     if query:
-        if selected_field == "Physics":  
-            articles = Articles.objects.filter(title__icontains=query, field='physics')
-        elif selected_field == "Math":  
-            articles = Articles.objects.filter(title__icontains=query, field='math')
-        elif selected_field == "Economics":  
-            articles = Articles.objects.filter(title__icontains=query, field='economics')
-        elif selected_field == "Chemistry":  
-            articles = Articles.objects.filter(title__icontains=query, field='chemistry')
-        elif selected_field == "Biology":  
-            articles = Articles.objects.filter(title__icontains=query, field='biology')
-        elif selected_field == "Machine Learning":  
-            articles = Articles.objects.filter(title__icontains=query, field='machine learning')
+        if selected_field:
+            field_value = next((field[0] for field in Articles.CATEGORY_CHOICES if field[1] == selected_field), None)
+            if field_value:
+                articles = Articles.objects.filter(title__icontains=query, field=field_value)
+            else:
+                articles = Articles.objects.filter(title__icontains=query)
         else:
             articles = Articles.objects.filter(title__icontains=query)
-    else:
-        articles = Articles.objects.none()
-
+    
     return render(request, 'search/papers.html', {"articles": articles})
